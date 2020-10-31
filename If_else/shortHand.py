aaccf3a088558905f8e70fe35e61734b7934ea05
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2020-10-31 15:51:47 +0530

    Short Hand

---
+++ If_else/shortHand.py
@@ -0,0 +1,14 @@
+#Short hand If
+print("Short hand If:")
+a = 8
+b = 6
+if a > b : print("Good Morning....!")
+print(" ")
+
+#Short hand Else 
+print("Short hand Else:")
+a = 6
+b = 8
+print("A") if a > b else print("B")
+print(" ")
+
