02efe9e2a4e33256e8992f28d6f235e76b136fbb
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2020-10-31 14:21:05 +0530

    Boolean Expressions in Python

--- Boolean/compareTwoNumbers.py
+++ Boolean/compareTwoNumbers.py
@@ -1,6 +1,16 @@
 #When we compare two numbers, the expression is evaluated and the Python 
 # returns a Boolean answer TRUE or FALSE 
-
+print("Boolean expressions :")
 print(10 > 9) #returns TRUE
 print(10 == 9) #returns FALSE
 print(10 < 9) #returns FALSE
+
+print(" ")
+#Python returns TRUE or FALSE in an if statement 
+print("Use of Boolean in if-else:")
+a = 5
+b = 60
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
