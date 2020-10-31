512261544c187c0a68ad3e73ea90d7fe90250db0
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2020-10-31 16:01:06 +0530

    Loops

---
+++ Loops/whileAndFor.py
@@ -0,0 +1,16 @@
+#WHILE LOOP
+#print i as long as i <= 6 using while loop
+print("Printing i as long as i <= 6 using while loop:")
+i = 1
+while i <= 6:
+  print(i)
+  i = i + 1
+
+print(" ")
+#FOR LOOP
+#printing i as long as i <= 6 using for loop
+print("printing i as long as i <= 6 using for loop:")
+i = 1 
+for i in range(6):  #The range() function returns a sequence of numbers, 
+  #starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+  print(i)
