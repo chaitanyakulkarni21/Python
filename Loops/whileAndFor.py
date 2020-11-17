4e55098f7f86b878af5fd1de94eddf417a9b6bf0
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2020-11-17 14:32:28 +0530

    Functions using Python

--- Loops/whileAndFor.py
+++ Loops/whileAndFor.py
@@ -14,3 +14,12 @@ i = 1
 for i in range(6):  #The range() function returns a sequence of numbers, 
   #starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
   print(i)
+
+print(" ")
+#print * 5 times 
+print("Printing * 5 times : ")
+i = 1
+while i <= 5:
+  print("*")
+  i = i + 1
+
